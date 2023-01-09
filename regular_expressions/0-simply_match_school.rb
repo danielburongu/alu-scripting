d95bd0527064b487ae218523a8812d8e87dc88c4
danielburongu <d.burongu@alustudent.com>
2023-01-10 01:34:39 +0300

    added to codes to 0 file

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -7,8 +7,8 @@ regex = /School/
 
 # Applying the match method to check if the input matches the regular expression
 if input.match(regex)
-  puts "'School'"
+  puts "School"
 else
-  puts 
+  puts "SchoolSchool"
 end
 ruby script.rb "School"
