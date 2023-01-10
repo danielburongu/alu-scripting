8e1a5095e09da1a7fb50d0dd1dcc427c9d974e08
danielburongu <d.burongu@alustudent.com>
2023-01-10 17:34:34 +0300

    updates on files.

--- regular_expressions/3-repetition_token_2.rb
+++ regular_expressions/3-repetition_token_2.rb
@@ -7,8 +7,8 @@ regex = /your-regex-here/
 
 # applying  the `match` method to check whether the string matches the regular expression
 if string.match(regex)
-  puts "holberton found!"
+  puts "hbn"
 else
-  puts "holberton not found."
+  puts "hbtttttn"
 end
-ruby regex_match.rb "holberton"
+ruby regex_match.rb "hbtn"
