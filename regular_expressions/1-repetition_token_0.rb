527cf579c4297890864ae72d61463a821467b315
danielburongu <d.burongu@alustudent.com>
2023-01-11 00:38:10 +0300

    updates on files.

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,14 +1,6 @@
 #!/usr/bin/env ruby
 # Using one command line argument
-string = ARGV[0]
 puts ARGV[0].scan(/hbt{2,5}n/).join
 # regular expression used to match the string
-regex = /your-regex-here/
-
 # Using the `match` method to check whether the string matches the regular expression
-if string.match(regex)
-  puts "hbtttn"
-else
-  puts "hbttn"
-end
-ruby regex_match.rb "hbttn"
+
