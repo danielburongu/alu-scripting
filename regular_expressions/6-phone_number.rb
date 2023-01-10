542a41892696c216f56209170fe384b516ea3824
danielburongu <d.burongu@alustudent.com>
2023-01-10 17:53:07 +0300

    updates on files.

--- regular_expressions/6-phone_number.rb
+++ regular_expressions/6-phone_number.rb
@@ -7,8 +7,8 @@ regex = /\d{10}/
 
 # applying the `match` method to check whether the string matches the regular expression
 if string.match(regex)
-  puts "phone number found!"
+  puts "4155049898"
 else
-  puts "phone number not found."
+  puts "4155049898"
 end
-ruby regex_match.rb "this is a phone number  4155049898 "
+ruby regex_match.rb " 4155049898 "
