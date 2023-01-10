22c105abccf703e2ea3cc0172b574cd975eb9db5
danielburongu <d.burongu@alustudent.com>
2023-01-10 17:28:29 +0300

    updates on files.

--- regular_expressions/2-repetition_token_1.rb
+++ regular_expressions/2-repetition_token_1.rb
@@ -7,8 +7,8 @@ regex = /your-regex-here/
 
 # applying the `match` method to check whether the string matches the regular expression
 if string.match(regex)
-  puts "got holberton!"
+  puts "htn"
 else
-  puts "did not get."
+  puts "hbtn"
 end
-ruby regex_match.rb "holberton"
+ruby regex_match.rb "hbbbbtn"
