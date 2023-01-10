d25c344b8ae1bfdbc3543da581c1fe1815d78f92
danielburongu <d.burongu@alustudent.com>
2023-01-10 17:14:15 +0300

    updates on files.

--- regular_expressions/5-beginning_and_end.rb
+++ regular_expressions/5-beginning_and_end.rb
@@ -7,8 +7,8 @@ regex = /^h.n$/
 
 # applying the `match` method to check whether the string matches the regular expression
 if string.match(regex)
-  puts "holberton found!"
+  puts "hhn"
 else
-  puts "holberton not found."
+  puts "h9n"
 end
-ruby regex_match.rb "holberton"
+ruby regex_match.rb "Hhn"
