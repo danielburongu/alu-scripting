40847c9985199b89406fee7cfadae1477f68a00d
danielburongu <d.burongu@alustudent.com>
2023-01-11 00:13:41 +0300

    updates on files.

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,14 +1,4 @@
 #!/usr/bin/env ruby
 # The first argument passed to the script in a variable
-input = ARGV[0].scan(/School/).join
+puts ARGV[0].scan(/School/).join
 
-# Creating a regular expression to match the word "School"
-regex = /School/
-
-# Applying the match method to check if the input matches the regular expression
-if input.match(regex)
-  puts "School"
-else
-  puts 
-end
-ruby script.rb "SchoolSchool"
