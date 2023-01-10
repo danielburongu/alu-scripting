362b4313109a601d724889519782f2e14c8fb06d
danielburongu <d.burongu@alustudent.com>
2023-01-10 18:00:01 +0300

    updates on files.

--- regular_expressions/6-phone_number.rb
+++ regular_expressions/6-phone_number.rb
@@ -7,8 +7,8 @@ regex = /\d{10}/
 
 # applying the `match` method to check whether the string matches the regular expression
 if string.match(regex)
-  puts "4155049898"
+  puts ""
 else
   puts "4155049898"
 end
-ruby regex_match.rb ""
+ruby regex_match.rb " 4155049898 "
