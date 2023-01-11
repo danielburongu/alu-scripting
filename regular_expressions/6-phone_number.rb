b39a7d104b251138974ac0fc91532dd7608d8489
danielburongu <d.burongu@alustudent.com>
2023-01-11 09:47:11 +0300

    updates on files.

--- regular_expressions/6-phone_number.rb
+++ regular_expressions/6-phone_number.rb
@@ -1,8 +1,5 @@
 #!/usr/bin/env ruby
 # Uisng one command line argument
-string = ARGV[0]
-
 # the regular expression
-regex = /^\d{10}$/
-# applying the `match` method to check whether the string matches the regular expression
-
+# applying the `scan and join` method to check whether the string matches the regular expression
+puts ARGV[0].scan(/^\d{10}$/).join
