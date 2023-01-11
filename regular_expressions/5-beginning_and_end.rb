8dcabe40dff4ed9626991dec02023a833b707ff3
danielburongu <d.burongu@alustudent.com>
2023-01-11 09:35:21 +0300

    updates on files.

--- regular_expressions/5-beginning_and_end.rb
+++ regular_expressions/5-beginning_and_end.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 # using the command line argument
+string = ARGV[0]
+
 #the regular expression
-# applying the `scan and join` method to check whether the string matches the regular expression
-puts ARGV[0].scan(/h.n/).join
+regex = /h.n/
+
+# applying the `match` method to check whether the string matches the regular expression
+
