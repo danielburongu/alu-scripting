b4489017fc18e2727779eee76b29bfa9e81b6174
danielburongu <d.burongu@alustudent.com>
2023-03-06 18:43:42 +0300

    updates

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,20 +5,16 @@ import requests
 
 # Define the function `number_of_subscribers` that takes a subreddit
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
+    """"Doc"""
+    url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
 
-# Create the URL for the subreddit's about.json page using string formatting
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
 
-    # Send a GET request to the subreddit's about.json page with a 'User-Agent'
-    res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-
-    # If the response status code is not 200, return 0
     if res.status_code != 200:
         return 0
-
-    # Extract JSON data from the response and return the number of subscribers
     else:
         json_response = res.json()
-        subscribers = json_response.get('data').get('subscribers')
-        return subscribers
+        return json_response.get('data').get('subscribers')
