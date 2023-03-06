09a5677495a0b3ab9398db820f683e0dc5da6a7a
danielburongu <d.burongu@alustudent.com>
2023-03-06 18:47:29 +0300

    updates

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""""Doc"""
+""""Documentation"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """"Doc"""
+    """"Documentation"""
     url = "https://www.reddit.com/r/{}/about.json" \
         .format(subreddit)
 
