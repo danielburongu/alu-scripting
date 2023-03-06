8a37ac0f1bb210209981d1739c255608c1854b6f
danielburongu <d.burongu@alustudent.com>
2023-03-06 19:03:38 +0300

    updates

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,17 +4,17 @@ import requests
 
 
 def top_ten(subreddit):
-    """"Documentation for top_ten function"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    """""Documentation for top_ten function"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
 
-    headers = {'User-Agent': 'Mozilla/5.0'}
-   
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
 
-    if response.status_code != 200:
-        print("None")
-        return
-
-    data = response.json()
-    for post in data['data']['children']:
-        print(post['data']['title'])
+    if res.status_code != 200:
+        print(None)
+    else:
+        json_response = res.json()
+        posts = json_response.get('data').get('children')
+        [print(post.get('data').get('title')) for post in posts]
