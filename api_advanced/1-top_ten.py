4495ef15f25ddeb702f5999234aca1f6a3b8768c
danielburongu <d.burongu@alustudent.com>
2023-03-06 19:00:08 +0300

    updates

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,10 +2,13 @@
 """"Documentation for top_ten module"""
 import requests
 
+
 def top_ten(subreddit):
     """"Documentation for top_ten function"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+
     headers = {'User-Agent': 'Mozilla/5.0'}
+   
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
