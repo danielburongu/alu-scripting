9310a75c528ed0a1b377637e4157c3d63f4fbe1f
danielburongu <d.burongu@alustudent.com>
2023-03-06 18:37:03 +0300

    updates

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,20 +7,18 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
 
-
-# Create the URL for the subreddit's about.json page using string formatting
+    # Create the URL for the subreddit's about.json page using string formatting
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-# Send a GET request to the subreddit's about.json page with a 'User-Agent'
+    # Send a GET request to the subreddit's about.json page with a 'User-Agent'
     res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
-# If the response status code is not 200, return 0
+    # If the response status code is not 200, return 0
     if res.status_code != 200:
         return 0
-# extract JSON data from the response and return the number of subscribers
+
+    # Extract JSON data from the response and return the number of subscribers
     else:
-        """Convert JSON response to a Python dictionary `json()` method"""
         json_response = res.json()
-# Extract the number of subscribers JSON response using the `get()` method
         subscribers = json_response.get('data').get('subscribers')
         return subscribers
